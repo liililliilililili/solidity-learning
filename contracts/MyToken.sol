5373b5d9b3dc4ed7c47b0ba6e564ee7fb39f280c
github hilee <haein8378@gmail.com>
2025-10-12 03:49:26 +0900

    added transfer

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifer: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
 contract MyToken {
@@ -9,21 +9,27 @@ contract MyToken {
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimal) {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimal,
+        uint256 _amount
+    ) {
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
+        _mint(_amount * 10 ** uint256(decimals), msg.sender); // 1 MT
     }
 
-    // function totalSupply() external view returns (uint256) {
-    //     return totalSupply;
-    // }
+    function _mint(uint256 amount, address owner) internal {
+        totalSupply += amount;
+        balanceOf[owner] += amount;
+    }
 
-    // function balanceOf(address owner) external view returns (uint256) {
-    //     return balanceOf(owner);
-    // }
+    function transfer(uint256 amount, address to) external {
+        require(balanceOf[msg.sender] >= amount, "insufficient balance");
 
-    // function name() external view returns (string memory) {
-    //     return name;
-    // }
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+    }
 }
